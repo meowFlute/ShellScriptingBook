d808f95fc6b2188be53616181a92d847b4763c74
Scott Christensen <mscottchristensen@gmail.com>
2023-05-17 09:04:17 -0400

    read a little more of chapter 3 regarding reading files using read

--- scripts/ch03_variables_examples.sh
+++ scripts/ch03_variables_examples.sh
@@ -11,6 +11,7 @@ echo "The input you entered was: $user_input0"
 
 # read can be used to bring in multiple variables
 # The last variable will take up all unread text
+echo
 echo -n "Now please enter two or more words: "
 
 read user_input1 user_input2
@@ -19,5 +20,21 @@ echo "The first word you entered was: $user_input1, and the second group include
 
 # Just set will list all the environment variables
 # this means you can grep that list to see values you set previously
-echo "relevant variables:"
+echo
+echo "Relevant variables:"
 set | grep user_input
+
+# read will also read a single line in
+echo
+echo "The first line of /etc/fstab is:"
+read line < /etc/fstab
+echo $line
+
+# the return value of read is such that you can use it in a while loop because it returns nonzero for EOL
+# it will only quit on its own by returning 0 when it hits EOF
+echo
+echo "The whole file of /etc/fstab is:"
+while read line
+do
+    echo $line
+done < /etc/fstab
