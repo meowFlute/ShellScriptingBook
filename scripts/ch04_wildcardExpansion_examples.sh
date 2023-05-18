ffdc5a4909e6be69e10ab774e1bd2c8d3c4bc023
Scott Christensen <mscottchristensen@gmail.com>
2023-05-18 15:23:51 -0400

    added character class information to ch04

--- scripts/ch04_wildcardExpansion_examples.sh
+++ scripts/ch04_wildcardExpansion_examples.sh
@@ -17,4 +17,19 @@ echo "ls ch{03,04}*: $(ls ch{03,04}*)"
 # some parts of regular expressions also work for matching
 echo "ls ch[0-9]*: $(ls ch[0-9]*)" # NOTE: The '*' in this example is a glob, not a regex *
 
+# There exist character classes, but note that they are expanded including their own set of brackets, so they need to be enclosed within brackets
+# [[:alpha:]] = [[:lower:]] and [[:upper:]] = [A-Za-z]
+# [[:alnum:]] = [[:alpha:]] and [[:digit:]] = [0-9A-Za-z]
+# [[:blank:]] = Blank characters: space and tab.
+# [[:cntrl:]] = Control characters. In ASCII, these characters have octal codes 000 through 037, and 177 (DEL).
+# [[:digit:]] = [0-9]
+# [[:graph:]] = [:alnum:] and [:punct:]
+# [[:lower:]] = [a-z]
+# [[:print:]] = [:alnum:], [:punct:], and [:space:]
+# [[:punct:]] = [! " # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` { | } ~.]
+# [[:space:] = Space characters: in the ‘C’ locale, this is tab, newline, vertical tab, form feed, carriage return, and space.
+# [[:upper:]] = [A-Z]
+# [[:xdigit:]] = Hexadecimal digits: 0 1 2 3 4 5 6 7 8 9 A B C D E F a b c d e f.
+
+# You can disable globbing like below in this trivial example of naming and removing a dumb filename that would otherwise destroy what we have
 
